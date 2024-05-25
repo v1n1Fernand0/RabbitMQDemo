14f172350eb0a6eb55c663bb15f1d9b1e8a8b8c7
Vinicius Souza Fernando <vinicius.sfernando@hotmail.com>
2024-05-24 22:16:32 -0300

    Adicionar arquivos de projeto.

---
+++ RabbitReceiver2/Program.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672"),
+    ClientProvidedName = "Rabbit Receiver2 App"
+};
+
+IConnection connection = factory.CreateConnection();
+
+IModel channel = connection.CreateModel();
+
+string exchangeName = "demo.exchange";
+string routingKey = "demo.key";
+string queueName = "demo.queue";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, false, false, false, null);
+channel.QueueBind(queueName, exchangeName, routingKey, null);
+
+channel.BasicQos(0, 1, false);
+
+EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    Task.Delay(3000).Wait();  // delay increased to 3 seconds
+    byte[] body = ea.Body.ToArray();
+    string message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Received message: {message}");
+
+    channel.BasicAck(ea.DeliveryTag, false);
+};
+
+string consumerTag = channel.BasicConsume(queueName, false, consumer);
+Console.ReadLine();
+
+channel.BasicCancel(consumerTag);
+channel.Close();
+connection.Close();
---
+++ RabbitSender/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+
+ConnectionFactory factory = new ConnectionFactory
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672"),
+    ClientProvidedName = "Rabbit Sender App"
+};
+
+IConnection connection = factory.CreateConnection();
+
+IModel channel = connection.CreateModel();
+
+string exchangeName = "demo.exchange";
+string routingKey = "demo.key";
+string queueName = "demo.queue";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, false, false, false, null);
+channel.QueueBind(queueName, exchangeName, routingKey, null);
+
+for (int i = 0; i < 60; i++)
+{
+    Console.WriteLine($"Escrevendo mensagem de número: {i}");
+    byte[] messageBodyBytes = Encoding.UTF8.GetBytes($"Hello, RabbitMQ {i}!");
+    channel.BasicPublish(exchangeName, routingKey, null, messageBodyBytes);
+}
+
+channel.Close();
+connection.Close();
+ 
